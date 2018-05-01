2801dc27124c664cfa78f37fd6402395cab62c06
spasea <ithespv@gmail.com>
2018-05-01 12:54:06 +0300

    add transpiled version

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /**
  * @param {Object} options
  *        {string} options.elementSym = '__'
  *        {string} options.modificationSym = '--'
  *        {string} options.baseClass = ''
  */
-class Classes {
-  constructor (options) {
-    options = {
+var Classes = function () {
+  function Classes(options) {
+    _classCallCheck(this, Classes);
+
+    options = _extends({
       elementSym: '__',
       modificationSym: '--',
-      baseClass: '',
-      ...options
-    }
+      baseClass: ''
+    }, options);
 
-    this.baseClass = options.baseClass
-    this.elementSym = options.elementSym
-    this.modificationSym = options.modificationSym
-    this.classesArr = []
+    this.baseClass = options.baseClass;
+    this.elementSym = options.elementSym;
+    this.modificationSym = options.modificationSym;
+    this.classesArr = [];
   }
 
   /**
@@ -25,104 +42,126 @@ class Classes {
    *        options.m {Array|Object|String|null}
    * @returns {{className: string}}
    */
-  result (options = undefined) {
-    if (options === undefined) {
-      return {className: this.baseClass}
-    }
 
-    const isElement = Object.keys(options)[0] !== 'm'
-    const isString = typeof options === "string"
-    const isArray = options.length === undefined
 
-    this.classesArr = []
+  _createClass(Classes, [{
+    key: 'result',
+    value: function result() {
+      var _this = this;
 
-    if (isArray) {
-      options = [options]
-    }
+      var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : undefined;
 
-    if (isString) {
-      options = [{
-        [options]: true
-      }]
-    }
+      if (options === undefined) {
+        return { className: this.baseClass };
+      }
 
-    options.forEach(item => {
-      let keys = Object.keys(item)
-      let element = keys[0]
-      let modifiers = item.m
+      var isElement = Object.keys(options)[0] !== 'm';
+      var isString = typeof options === "string";
+      var isArray = options.length === undefined;
 
-      if (isElement) {
-        if (!item[element]) {
-          return
+      this.classesArr = [];
+
+      if (isArray) {
+        options = [options];
+      }
+
+      if (isString) {
+        options = [_defineProperty({}, options, true)];
+      }
+
+      options.forEach(function (item) {
+        var keys = Object.keys(item);
+        var element = keys[0];
+        var modifiers = item.m;
+
+        if (isElement) {
+          if (!item[element]) {
+            return;
+          }
+
+          _this.classesArr.push(element);
+
+          var _resultingModifiers = _this.solveModifiers(modifiers, element);
+
+          if (_resultingModifiers.length) {
+            var _classesArr;
+
+            (_classesArr = _this.classesArr).push.apply(_classesArr, _toConsumableArray(_resultingModifiers));
+          }
+
+          _this.classesArr = _this.classesArr.map(function (item) {
+            return [_this.baseClass, item].join(_this.elementSym);
+          });
+
+          return;
         }
 
-        this.classesArr.push(element)
+        if (!_this.classesArr.includes(_this.baseClass)) {
+          _this.classesArr.push(_this.baseClass);
+        }
 
-        let resultingModifiers = this.solveModifiers(modifiers, element)
+        var resultingModifiers = _this.solveModifiers(modifiers, _this.baseClass);
 
         if (resultingModifiers.length) {
-          this.classesArr.push(...resultingModifiers)
+          var _classesArr2;
+
+          (_classesArr2 = _this.classesArr).push.apply(_classesArr2, _toConsumableArray(resultingModifiers));
+        }
+      });
+
+      return { className: this.classesArr.join(' ').trim() };
+    }
+  }, {
+    key: 'solveModifiers',
+    value: function solveModifiers(modifiers, element) {
+      var _this2 = this;
+
+      var classesArr = [];
+
+      if (modifiers) {
+        var _classesArr3;
+
+        if (modifiers.length === undefined) {
+          Object.keys(modifiers).map(function (inner) {
+            return modifiers[inner] ? classesArr.push([element, inner].join(_this2.modificationSym)) : null;
+          });
+
+          return classesArr;
         }
 
-        this.classesArr = this.classesArr.map(item => [this.baseClass, item].join(this.elementSym))
+        if (typeof modifiers === "string") {
+          modifiers = [modifiers];
+        }
 
-        return
+        (_classesArr3 = this.classesArr).push.apply(_classesArr3, _toConsumableArray(modifiers.map(function (item) {
+          return [element, item].join(_this2.modificationSym);
+        })));
       }
 
-      if (!this.classesArr.includes(this.baseClass)) {
-        this.classesArr.push(this.baseClass)
-      }
-
-      let resultingModifiers = this.solveModifiers(modifiers, this.baseClass)
-
-      if (resultingModifiers.length) {
-        this.classesArr.push(...resultingModifiers)
-      }
-    })
-
-    return {className: this.classesArr.join(' ').trim()}
-  }
-
-  solveModifiers (modifiers, element) {
-    let classesArr = []
-
-    if (modifiers) {
-      if (modifiers.length === undefined) {
-        Object.keys(modifiers).map(inner => modifiers[inner]
-          ? classesArr.push([element, inner].join(this.modificationSym))
-          : null
-        )
-
-        return classesArr
-      }
-
-      if (typeof modifiers === "string") {
-        modifiers = [modifiers]
-      }
-
-      this.classesArr.push(...modifiers.map(item => [element, item].join(this.modificationSym)))
+      return classesArr;
     }
+  }], [{
+    key: 'getClassesList',
+    value: function getClassesList(list) {
+      var classesArr = [];
 
-    return classesArr
-  }
+      if (Array.isArray(list)) return list.join(' ');
 
-  static getClassesList (list) {
-    let classesArr = []
+      for (var key in list) {
+        if (list[key]) classesArr.push(key);
+      }
 
-    if (Array.isArray(list)) return list.join(' ')
-
-    for (let key in list) {
-      if (list[key]) classesArr.push(key)
+      return classesArr.join(' ');
     }
+  }, {
+    key: 'resultList',
+    value: function resultList(list) {
+      return { className: Classes.getClassesList(list) };
+    }
+  }]);
 
-    return classesArr.join(' ')
-  }
+  return Classes;
+}();
 
-  static resultList (list) {
-    return {className: Classes.getClassesList(list)}
-  }
-}
-
-export default Classes
-
-export const cls = new Classes()
+exports.default = Classes;
+var cls = exports.cls = new Classes();
