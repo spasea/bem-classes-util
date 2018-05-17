ca1337890644cdb95b7a741d2c37b96b7a385a03
spasea <ithespv@gmail.com>
2018-05-17 23:37:06 +0300

    working laravel mix version with js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,14 @@
 let mix = require('laravel-mix');
+const path = require("path");
 
-mix.babel('lib/index.js', './index.js')
+mix
+  // .webpackConfig({
+  //   resolve: {
+  //     alias: {
+  //       lib$: path.resolve(__dirname, 'lib/')
+  //     }
+  //   }
+  //   target: "node"
+  // })
+  // .setPublicPath('./')
+  .babel('lib/index.js', './index.js')
