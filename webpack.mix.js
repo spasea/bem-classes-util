aa1bd2ce5fd0f478caffc5b782900286bd68c6ac
spasea <ithespv@gmail.com>
2018-05-18 00:29:01 +0300

    add module resolver

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,4 @@
 let mix = require('laravel-mix');
-const path = require("path");
 
 mix
-  // .webpackConfig({
-  //   resolve: {
-  //     alias: {
-  //       lib$: path.resolve(__dirname, 'lib/')
-  //     }
-  //   }
-  //   target: "node"
-  // })
-  // .setPublicPath('./')
   .babel('lib/index.js', './index.js')
